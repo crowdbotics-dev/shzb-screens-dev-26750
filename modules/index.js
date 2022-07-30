b79dc85d36c97bdbc5e1b8a0b0da3b3745e4d087
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-07-31 02:19:46 +0500

    Add Review Screen Added

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from './*/index.js';
 import { getModules } from './modules.js';
 
 export const modules = getModules(mods);
-export const initialRoute = 'Addpaymentmethodscreen';
+export const initialRoute = 'Addreviewscreen';
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
