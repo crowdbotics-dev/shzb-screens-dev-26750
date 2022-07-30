ecbb532e7820a290832b7b4df2d1e1413af9fe2c
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-07-30 23:33:17 +0500

    Account Settings Screen Added

--- modules/index.js
+++ modules/index.js
@@ -1,29 +1,29 @@
-import mods from './*/index.js'
-import { getModules } from './modules.js'
+import mods from './*/index.js';
+import { getModules } from './modules.js';
 
-export const modules = getModules(mods)
-export const initialRoute = 'Abouttheappscreen'
+export const modules = getModules(mods);
+export const initialRoute = 'Accountsettingsscreen';
 export const slices = modules
   .filter(mod => mod.value.slice)
-  .map(mod => mod.value.slice)
+  .map(mod => mod.value.slice);
 export const reducers = slices.reduce((acc, slice) => {
-  let name = slice.name.charAt(0).toUpperCase() + slice.name.slice(1)
-  acc[name] = slice.reducer
-  return acc
-}, {})
+  let name = slice.name.charAt(0).toUpperCase() + slice.name.slice(1);
+  acc[name] = slice.reducer;
+  return acc;
+}, {});
 export const navigators = modules
   .filter(mod => mod.value.navigator)
   .map(mod => {
     return {
       name: mod.name,
-      value: mod.value.navigator
-    }
-  })
+      value: mod.value.navigator,
+    };
+  });
 export const hooks = modules
   .filter(mod => mod.value.hook)
   .map(mod => {
     return {
       name: mod.name,
-      value: mod.value.hook
-    }
-  })
+      value: mod.value.hook,
+    };
+  });
