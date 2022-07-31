e0d007719c70075b2ad85c8c6a22911ff5bde154
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-07-31 19:02:44 +0500

    Appointment Listing Screen Added

--- screens/Appointmentlistscreen/index.js
+++ screens/Appointmentlistscreen/index.js
@@ -1,65 +1,85 @@
 import React, { useState } from 'react';
-import { Text, StyleSheet, View, SafeAreaView, Image, FlatList } from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Image,
+  FlatList,
+} from 'react-native';
 
 const AppointmentListScreen = params => {
-  const [appointments, setAppointments] = useState([{
-    id: 1,
-    name: 'Appointment',
-    date: '17 June 2022',
-    time: '12:00 AM',
-    image: require('./assets/crowdbotics.png')
-  }, {
-    id: 2,
-    name: 'Appointment',
-    date: '17 May 2022',
-    time: '02:00 AM',
-    image: require('./assets/crowdbotics.png')
-  }, {
-    id: 3,
-    name: 'Appointment',
-    date: '17 April 2022',
-    time: '12:00 AM',
-    image: require('./assets/crowdbotics.png')
-  }, {
-    id: 4,
-    name: 'Appointment',
-    date: '17 March 2022',
-    time: '12:00 AM',
-    image: require('./assets/crowdbotics.png')
-  }, {
-    id: 5,
-    name: 'Appointment',
-    date: '17 March 2022',
-    time: '12:00 AM',
-    image: require('./assets/crowdbotics.png')
-  }, {
-    id: 6,
-    name: 'Appointment',
-    date: '17 March 2022',
-    time: '12:00 AM',
-    image: require('./assets/crowdbotics.png')
-  }, {
-    id: 7,
-    name: 'Appointment',
-    date: '17 March 2022',
-    time: '12:00 AM',
-    image: require('./assets/crowdbotics.png')
-  }]);
-  return <SafeAreaView style={styles.container}>
+  const [appointments, setAppointments] = useState([
+    {
+      id: 1,
+      name: 'Appointment',
+      date: '17 June 2022',
+      time: '12:00 AM',
+      image: require('./assets/crowdbotics.png'),
+    },
+    {
+      id: 2,
+      name: 'Appointment',
+      date: '17 May 2022',
+      time: '02:00 AM',
+      image: require('./assets/crowdbotics.png'),
+    },
+    {
+      id: 3,
+      name: 'Appointment',
+      date: '17 April 2022',
+      time: '12:00 AM',
+      image: require('./assets/crowdbotics.png'),
+    },
+    {
+      id: 4,
+      name: 'Appointment',
+      date: '17 March 2022',
+      time: '12:00 AM',
+      image: require('./assets/crowdbotics.png'),
+    },
+    {
+      id: 5,
+      name: 'Appointment',
+      date: '17 March 2022',
+      time: '12:00 AM',
+      image: require('./assets/crowdbotics.png'),
+    },
+    {
+      id: 6,
+      name: 'Appointment',
+      date: '17 March 2022',
+      time: '12:00 AM',
+      image: require('./assets/crowdbotics.png'),
+    },
+    {
+      id: 7,
+      name: 'Appointment',
+      date: '17 March 2022',
+      time: '12:00 AM',
+      image: require('./assets/crowdbotics.png'),
+    },
+  ]);
+  return (
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}>Appointments</Text>
         <Image source={require('./assets/calender.png')} />
       </View>
-      <FlatList style={styles.list} data={appointments} keyExtractor={appointment => appointment.id.toString()} renderItem={({
-      item
-    }) => <Appointment item={item} />} contentContainerStyle={styles.listContainer} />
-    </SafeAreaView>;
+      <FlatList
+        style={styles.list}
+        data={appointments}
+        keyExtractor={appointment => appointment.id.toString()}
+        renderItem={({ item }) => <Appointment item={item} />}
+        contentContainerStyle={styles.listContainer}
+      />
+    </SafeAreaView>
+  );
 };
 
-const Appointment = ({
-  item
-}) => {
-  return <View style={styles.appointmentContainer}>
+const Appointment = ({ item }) => {
+  return (
+    <View style={styles.appointmentContainer}>
       <View style={styles.imgContainer}>
         <Image source={item.image} style={styles.img} />
       </View>
@@ -71,35 +91,35 @@ const Appointment = ({
           <Text style={styles.timingText}>{item.time}</Text>
         </View>
       </View>
-    </View>;
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     padding: 10,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
   headerText: {
     fontSize: 18,
     fontWeight: 'bold',
     padding: 2,
-    marginVertical: 12
+    marginVertical: 12,
   },
   list: {
     padding: 10,
     paddingTop: 0,
-    marginHorizontal: 20
+    marginHorizontal: 20,
   },
   listContainer: {
-    margin: 0 // backgroundColor: '#f1f1f1',
-
+    margin: 0, // backgroundColor: '#f1f1f1',
   },
   appointmentContainer: {
     backgroundColor: '#fff',
@@ -112,7 +132,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 10,
     borderBottomWidth: 1,
-    borderColor: '#e6e6e6'
+    borderColor: '#e6e6e6',
   },
   imgContainer: {
     justifyContent: 'center',
@@ -120,19 +140,19 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     backgroundColor: '#ddd',
     height: 80,
-    width: 80
+    width: 80,
   },
   desContainer: {
     width: '70%',
     justifyContent: 'space-between',
     height: '60%',
-    alignItems: 'flex-start'
+    alignItems: 'flex-start',
   },
   timings: {
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
   timingText: {
-    color: 'grey'
-  }
+    color: 'grey',
+  },
 });
 export default AppointmentListScreen;
