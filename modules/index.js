a2acc78605d4694eb4f691cc8bc18f0da6cfe7f8
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-01 18:02:13 +0500

    Billingpreferencesscreen Added

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from './*/index.js';
 import { getModules } from './modules.js';
 
 export const modules = getModules(mods);
-export const initialRoute = 'Archivecontentscreen';
+export const initialRoute = 'Billingpreferencesscreen';
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
