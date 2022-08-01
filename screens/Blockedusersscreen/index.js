502c37103d2e9310a91abb3173d28c38d9d2995a
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-01 23:06:29 +0500

    Chatlistingscreen Added and Blockedusersscreen preview changed

--- screens/Blockedusersscreen/index.js
+++ screens/Blockedusersscreen/index.js
@@ -1,28 +1,53 @@
 import React, { useState } from 'react';
-import { Text, StyleSheet, View, SafeAreaView, TextInput, Image, FlatList, Pressable } from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  TextInput,
+  Image,
+  FlatList,
+  Pressable,
+} from 'react-native';
 
 const SocialBlockedUsersScreen = params => {
   const [followers, setFollowers] = useState(4513);
   const [username, setUsername] = useState('');
-  const [blockedUsers, setBlockedUsers] = useState([{
-    id: 1,
-    name: 'John Doe',
-    profileImage: require('./assets/profile.png')
-  }, {
-    id: 2,
-    name: 'Cody Fisher',
-    profileImage: require('./assets/profile.png')
-  }, {
-    id: 3,
-    name: 'Jenny Wilson',
-    profileImage: require('./assets/profile.png')
-  }]);
-  return <SafeAreaView style={styles.container}>
+  const [blockedUsers, setBlockedUsers] = useState([
+    {
+      id: 1,
+      name: 'John Doe',
+      profileImage: require('./assets/profile.png'),
+    },
+    {
+      id: 2,
+      name: 'Cody Fisher',
+      profileImage: require('./assets/profile.png'),
+    },
+    {
+      id: 3,
+      name: 'Jenny Wilson',
+      profileImage: require('./assets/profile.png'),
+    },
+  ]);
+  return (
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <View style={styles.inputContainer}>
           <Text style={styles.inputText}>Search</Text>
-          <TextInput style={styles.input} onChangeText={text => setUsername(text)} value={username} placeholder="Search Username" placeholderTextColor="#9B9B9B" autoCapitalize="none" autoCorrect={false} />
-          <Image source={require('./assets/Vector.png')} style={styles.searchIcon} />
+          <TextInput
+            style={styles.input}
+            onChangeText={text => setUsername(text)}
+            value={username}
+            placeholder="Search Username"
+            placeholderTextColor="#9B9B9B"
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+          <Image
+            source={require('./assets/Vector.png')}
+            style={styles.searchIcon}
+          />
         </View>
       </View>
       <View style={styles.follower_blocked}>
@@ -32,16 +57,18 @@ const SocialBlockedUsersScreen = params => {
       <View style={styles.frequentBar}>
         <Text style={[styles.fnt16, styles.bold, styles.grey]}>Frequently</Text>
       </View>
-      <FlatList data={blockedUsers} keyExtractor={(item, index) => item.id.toString()} renderItem={({
-      item
-    }) => <User user={item} />} />
-    </SafeAreaView>;
+      <FlatList
+        data={blockedUsers}
+        keyExtractor={(item, index) => item.id.toString()}
+        renderItem={({ item }) => <User user={item} />}
+      />
+    </SafeAreaView>
+  );
 };
 
-const User = ({
-  user
-}) => {
-  return <View style={styles.userContainer}>
+const User = ({ user }) => {
+  return (
+    <View style={styles.userContainer}>
       <View style={styles.userInfo}>
         <Image source={user.profileImage} style={styles.profileImage} />
         <Text style={styles.username}>{user.name}</Text>
@@ -49,30 +76,31 @@ const User = ({
       <Pressable style={styles.btnContainer}>
         <Text style={styles.btntext}>Unblock</Text>
       </Pressable>
-    </View>;
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'flex-start'
+    justifyContent: 'flex-start',
   },
   header: {
     padding: 20,
     // flex: 1,
-    height: 100
+    height: 100,
   },
   inputContainer: {
     flexDirection: 'column',
     flex: 1,
     justifyContent: 'center',
-    marginHorizontal: 5
+    marginHorizontal: 5,
   },
   inputText: {
     fontSize: 16,
     marginLeft: 20,
-    color: '#111112'
+    color: '#111112',
   },
   input: {
     borderWidth: 1,
@@ -82,35 +110,35 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     marginVertical: 10,
     width: '100%',
-    height: 50
+    height: 50,
   },
   searchIcon: {
     position: 'absolute',
     right: 30,
-    top: 35
+    top: 35,
   },
   follower_blocked: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingHorizontal: 30
+    paddingHorizontal: 30,
   },
   frequentBar: {
     paddingHorizontal: 40,
     paddingVertical: 15,
     backgroundColor: '#e6e6e6',
-    marginVertical: 10
+    marginVertical: 10,
   },
   fnt16: {
-    fontSize: 16
+    fontSize: 16,
   },
   bold: {
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   grey: {
-    color: 'grey'
+    color: 'grey',
   },
   black: {
-    color: 'black'
+    color: 'black',
   },
   userContainer: {
     flexDirection: 'row',
@@ -120,32 +148,32 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#e6e6e6',
     marginHorizontal: 20,
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   userInfo: {
     flex: 2,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-start'
+    justifyContent: 'flex-start',
   },
   profileImage: {
     borderRadius: 50,
     width: 70,
     height: 70,
-    resizeMode: 'contain'
+    resizeMode: 'contain',
   },
   username: {
     fontSize: 16,
     marginLeft: 20,
-    color: '#111112'
+    color: '#111112',
   },
   btnContainer: {
     flex: 1,
-    alignItems: 'flex-end'
+    alignItems: 'flex-end',
   },
   btntext: {
     fontSize: 17,
-    color: '#12D790'
-  }
+    color: '#12D790',
+  },
 });
 export default SocialBlockedUsersScreen;
