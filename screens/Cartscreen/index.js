390def1dbdc256e3d7bd65252071f2cd540c69ec
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-02 00:28:09 +0500

    Createappointmentscreen Added

--- screens/Cartscreen/index.js
+++ screens/Cartscreen/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { Text, Image, StyleSheet, View, TouchableHighlight, ScrollView } from 'react-native';
+import {
+  Text,
+  Image,
+  StyleSheet,
+  View,
+  TouchableHighlight,
+  ScrollView,
+} from 'react-native';
 
 const ShoppingCartScreen = () => {
-  return <ScrollView>
+  return (
+    <ScrollView>
       <View style={styles.container}>
         <View style={styles.headerCard}>
           <View style={styles.cardContent}>
@@ -28,7 +36,11 @@ const ShoppingCartScreen = () => {
         </View>
         <View style={styles.cardContent}>
           <Text style={styles.chartText}>Chart</Text>
-          <Image resizeMode="contain" style={styles.cartImage} source={require('./assets/cart.png')} />
+          <Image
+            resizeMode="contain"
+            style={styles.cartImage}
+            source={require('./assets/cart.png')}
+          />
         </View>
         <OrderCard />
         <OrderCard />
@@ -37,14 +49,15 @@ const ShoppingCartScreen = () => {
           <Button>Checkout</Button>
         </View>
       </View>
-    </ScrollView>;
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     padding: 10,
     flex: 1,
-    backgroundColor: '#FFF'
+    backgroundColor: '#FFF',
   },
   headerCard: {
     paddingHorizontal: 30,
@@ -53,31 +66,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     shadowColor: 'rgba(0, 0, 0, 0.4)',
     elevation: 15,
-    borderRadius: 8
+    borderRadius: 8,
   },
   cardContent: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginVertical: 3
+    marginVertical: 3,
   },
   cardTextOne: {
     fontSize: 13,
-    color: '#424347'
+    color: '#424347',
   },
   cardTextTwo: {
     fontSize: 16,
     fontWeight: 'bold',
-    color: '#424347'
+    color: '#424347',
   },
   cartImage: {
     width: 31,
-    height: 23
+    height: 23,
   },
   chartText: {
     fontSize: 14,
     fontWeight: 'bold',
-    color: '#1E2022'
+    color: '#1E2022',
   },
   tabView: {
     width: '70%',
@@ -87,7 +100,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 10,
     padding: 6,
-    marginVertical: 20
+    marginVertical: 20,
   },
   selectedTab: {
     borderRadius: 10,
@@ -97,7 +110,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     shadowColor: 'gray',
-    elevation: 10
+    elevation: 10,
   },
   tabItem: {
     width: '50%',
@@ -105,17 +118,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F1F1F1',
-    borderRadius: 10
+    borderRadius: 10,
   },
   btnContainer: {
     marginVertical: 20,
-    paddingHorizontal: '10%'
-  }
+    paddingHorizontal: '10%',
+  },
 });
 export default ShoppingCartScreen;
 
 const OrderCard = () => {
-  return <View style={orderStyles.container}>
+  return (
+    <View style={orderStyles.container}>
       <View style={orderStyles.order}>
         <View style={orderStyles.image}>
           <Image source={require('./assets/edit.png')} />
@@ -125,19 +139,26 @@ const OrderCard = () => {
           <View style={orderStyles.bottomComponent}>
             <View style={orderStyles.quantity}>
               <Text>-</Text>
-              <Text style={{
-              fontWeight: 'bold'
-            }}>3</Text>
+              <Text
+                style={{
+                  fontWeight: 'bold',
+                }}>
+                3
+              </Text>
               <Text>+</Text>
             </View>
-            <Image source={require('./assets/delete.png')} style={orderStyles.img} />
+            <Image
+              source={require('./assets/delete.png')}
+              style={orderStyles.img}
+            />
           </View>
         </View>
       </View>
       <View>
         <Text style={orderStyles.orderPrice}>$10.25</Text>
       </View>
-    </View>;
+    </View>
+  );
 };
 
 const orderStyles = StyleSheet.create({
@@ -150,12 +171,12 @@ const orderStyles = StyleSheet.create({
     backgroundColor: '#FFF',
     shadowColor: 'rgba(0, 0, 0, 0.2)',
     elevation: 15,
-    borderRadius: 10
+    borderRadius: 10,
   },
   order: {
     flexDirection: 'row',
     alignItems: 'center',
-    padding: 5
+    padding: 5,
   },
   image: {
     height: 60,
@@ -164,11 +185,11 @@ const orderStyles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.2)',
     justifyContent: 'center',
     alignItems: 'center',
-    marginRight: 10
+    marginRight: 10,
   },
   description: {
     flexDirection: 'column',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   quantity: {
     width: 80,
@@ -179,38 +200,50 @@ const orderStyles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 10,
-    marginTop: 12
+    marginTop: 12,
   },
   bottomComponent: {
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   img: {
     marginLeft: 10,
-    marginTop: 10
+    marginTop: 10,
   },
   orderPrice: {
     fontSize: 30,
     fontWeight: 'bold',
-    marginRight: 10
-  }
+    marginRight: 10,
+  },
 });
 
 const Button = props => {
-  return <TouchableHighlight onPress={props.onPress} underlayColor="#DDDDDD">
-      <View style={[btnStyles.button, {
-      backgroundColor: props.backgroundColor ? props.backgroundColor : '#000000',
-      height: props.height ? props.height : 49,
-      borderWidth: props.borderWidth ? props.borderWidth : 0,
-      borderColor: props.borderColor ? props.borderColor : '#000000'
-    }]}>
-        <Text style={[btnStyles.text, {
-        color: props.color ? props.color : '#ffffff'
-      }]}>
+  return (
+    <TouchableHighlight onPress={props.onPress} underlayColor="#DDDDDD">
+      <View
+        style={[
+          btnStyles.button,
+          {
+            backgroundColor: props.backgroundColor
+              ? props.backgroundColor
+              : '#000000',
+            height: props.height ? props.height : 49,
+            borderWidth: props.borderWidth ? props.borderWidth : 0,
+            borderColor: props.borderColor ? props.borderColor : '#000000',
+          },
+        ]}>
+        <Text
+          style={[
+            btnStyles.text,
+            {
+              color: props.color ? props.color : '#ffffff',
+            },
+          ]}>
           {props.children}
         </Text>
       </View>
-    </TouchableHighlight>;
+    </TouchableHighlight>
+  );
 };
 
 const btnStyles = StyleSheet.create({
@@ -218,10 +251,10 @@ const btnStyles = StyleSheet.create({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 10
+    borderRadius: 10,
   },
   text: {
     fontWeight: 'bold',
-    fontSize: 15
-  }
+    fontSize: 15,
+  },
 });
