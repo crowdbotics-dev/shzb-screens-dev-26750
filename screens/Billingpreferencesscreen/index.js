943b963d84079cc921aa392924e59b810f67be89
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-02 01:42:05 +0500

    Createpostscreen Added

--- screens/Billingpreferencesscreen/index.js
+++ screens/Billingpreferencesscreen/index.js
@@ -1,23 +1,37 @@
 import React, { useState } from 'react';
-import { Text, StyleSheet, View, SafeAreaView, ScrollView, Image, TextInput, FlatList } from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TextInput,
+  FlatList,
+} from 'react-native';
 
 const BillingPreferencesScreen = params => {
   const [billingAddress, setBillingAddress] = useState('');
   const [shippingAddress, setShippingAddress] = useState('');
-  const [todayHistory, setTodayHistory] = useState([{
-    id: 1,
-    title: 'Sports Center',
-    price: '$125.90'
-  }, {
-    id: 2,
-    title: 'Shopping',
-    price: '$552.68'
-  }, {
-    id: 3,
-    title: 'Income',
-    price: '$345.00'
-  }]);
-  return <SafeAreaView style={styles.container}>
+  const [todayHistory, setTodayHistory] = useState([
+    {
+      id: 1,
+      title: 'Sports Center',
+      price: '$125.90',
+    },
+    {
+      id: 2,
+      title: 'Shopping',
+      price: '$552.68',
+    },
+    {
+      id: 3,
+      title: 'Income',
+      price: '$345.00',
+    },
+  ]);
+  return (
+    <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.header}>
           <View style={styles.paletteContainer}>
@@ -28,18 +42,43 @@ const BillingPreferencesScreen = params => {
               <Text>Add Card</Text>
             </View>
           </View>
-          <Image source={require('./assets/card.png')} style={styles.cardIcon} />
+          <Image
+            source={require('./assets/card.png')}
+            style={styles.cardIcon}
+          />
         </View>
         <View style={styles.cardInfo}>
-          <Image source={require('./assets/Card-large.png')} style={styles.card} />
-          <Image source={require('./assets/3Dots.png')} style={styles.threeDots} />
+          <Image
+            source={require('./assets/Card-large.png')}
+            style={styles.card}
+          />
+          <Image
+            source={require('./assets/3Dots.png')}
+            style={styles.threeDots}
+          />
           <View style={styles.inputContainer}>
             <Text style={styles.inputText}>Billing Address</Text>
-            <TextInput style={styles.input} onChangeText={text => setBillingAddress(text)} value={billingAddress} placeholder="Enter your Billing Address" placeholderTextColor="#9B9B9B" autoCapitalize="none" autoCorrect={false} />
+            <TextInput
+              style={styles.input}
+              onChangeText={text => setBillingAddress(text)}
+              value={billingAddress}
+              placeholder="Enter your Billing Address"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
           </View>
           <View style={styles.inputContainer}>
             <Text style={styles.inputText}>Shipping Address</Text>
-            <TextInput style={styles.input} onChangeText={text => setShippingAddress(text)} value={shippingAddress} placeholder="Enter your Shipping Address" placeholderTextColor="#9B9B9B" autoCapitalize="none" autoCorrect={false} />
+            <TextInput
+              style={styles.input}
+              onChangeText={text => setShippingAddress(text)}
+              value={shippingAddress}
+              placeholder="Enter your Shipping Address"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
           </View>
         </View>
         <View style={styles.history}>
@@ -53,30 +92,35 @@ const BillingPreferencesScreen = params => {
           </View>
           <View style={styles.historyList}>
             <Text style={styles.historyDate}>TODAY's</Text>
-            <FlatList data={todayHistory} renderItem={({
-            item
-          }) => <HistoryItem transaction={item} />} keyExtractor={item => item.id} />
+            <FlatList
+              data={todayHistory}
+              renderItem={({ item }) => <HistoryItem transaction={item} />}
+              keyExtractor={item => item.id}
+            />
           </View>
           <View style={styles.historyList}>
             <Text style={styles.historyDate}>APRIL 01</Text>
-            <FlatList data={todayHistory} renderItem={({
-            item
-          }) => <HistoryItem transaction={item} />} keyExtractor={item => item.id} />
+            <FlatList
+              data={todayHistory}
+              renderItem={({ item }) => <HistoryItem transaction={item} />}
+              keyExtractor={item => item.id}
+            />
           </View>
         </View>
       </ScrollView>
-    </SafeAreaView>;
+    </SafeAreaView>
+  );
 };
 
-const HistoryItem = ({
-  transaction
-}) => {
-  return <View style={styles.historyItem}>
+const HistoryItem = ({ transaction }) => {
+  return (
+    <View style={styles.historyItem}>
       <View style={styles.description}>
         <Text style={styles.titleText}>{transaction.title}</Text>
-        <Text style={{
-        color: 'grey'
-      }}>
+        <Text
+          style={{
+            color: 'grey',
+          }}>
           Invoice
         </Text>
       </View>
@@ -84,25 +128,26 @@ const HistoryItem = ({
         <Text style={styles.pricingText}>{transaction.price}</Text>
         <Image source={require('./assets/download.png')} />
       </View>
-    </View>;
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   header: {
     padding: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   cardIcon: {
     width: 30,
     height: 20,
     marginLeft: 20,
-    resizeMode: 'contain'
+    resizeMode: 'contain',
   },
   paletteContainer: {
     flexDirection: 'row',
@@ -114,7 +159,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'space-around',
-    paddingHorizontal: 15
+    paddingHorizontal: 15,
   },
   selected: {
     backgroundColor: '#fff',
@@ -122,32 +167,32 @@ const styles = StyleSheet.create({
     paddingHorizontal: 25,
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: '#e6e6e6'
+    borderColor: '#e6e6e6',
   },
   unSelected: {
     padding: 10,
-    paddingHorizontal: 25
+    paddingHorizontal: 25,
   },
   cardInfo: {
-    padding: 20
+    padding: 20,
   },
   card: {
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
   threeDots: {
     alignSelf: 'center',
-    marginVertical: 10
+    marginVertical: 10,
   },
   inputContainer: {
     flexDirection: 'column',
     flex: 1,
     justifyContent: 'center',
-    marginHorizontal: 5
+    marginHorizontal: 5,
   },
   inputText: {
     fontSize: 16,
     marginLeft: 20,
-    color: '#111112'
+    color: '#111112',
   },
   input: {
     borderWidth: 1,
@@ -156,23 +201,23 @@ const styles = StyleSheet.create({
     padding: 10,
     paddingLeft: 20,
     marginVertical: 10,
-    width: '100%'
+    width: '100%',
   },
   history: {
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
   historyDate: {
     fontSize: 16,
     marginVertical: 10,
     color: '#9B9B9B',
-    marginLeft: 20
+    marginLeft: 20,
   },
   historyList: {
     marginVertical: 10,
     borderWidth: 1,
     borderBottomWidth: 0,
     borderColor: '#e6e6e6',
-    borderRadius: 10
+    borderRadius: 10,
   },
   historyItem: {
     flexDirection: 'row',
@@ -182,29 +227,29 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     borderBottomWidth: 1,
     borderColor: '#e6e6e6',
-    height: 80
+    height: 80,
   },
   description: {
     flex: 3,
     flexDirection: 'column',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   titleText: {
     fontSize: 16,
-    color: '#111112'
+    color: '#111112',
   },
   pricing: {
     marginRight: 20,
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   pricingText: {
     marginRight: 10,
     fontSize: 16,
     color: '#111112',
-    fontWeight: 'bold'
-  }
+    fontWeight: 'bold',
+  },
 });
 export default BillingPreferencesScreen;
