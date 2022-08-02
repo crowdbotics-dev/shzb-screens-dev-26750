36ce0b2c29553a05af1294e87e83b0095553d566
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-03 03:51:14 +0500

    Ledgergraphscreen Added

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from './*/index.js';
 import { getModules } from './modules.js';
 
 export const modules = getModules(mods);
-export const initialRoute = 'Leaderboardscreen';
+export const initialRoute = 'Ledgergraphscreen';
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
--- screens/Ledgergraphscreen/index.js
+++ screens/Ledgergraphscreen/index.js
@@ -1,23 +1,81 @@
-import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import React from 'react';
+import { Image, Text, StyleSheet, View, ScrollView } from 'react-native';
 
 const LedgerGraphScreen = params => {
-  return <View style={styles.container}>
-      <Text style={styles.headingTxt}>Ledger Graph Screen</Text>
-    </View>;
+  return (
+    <ScrollView>
+      <View>
+        <View style={styles.container}>
+          <View>
+            <Text>Current Balance</Text>
+            <Text style={styles.currentBalance}>$35,559.00</Text>
+            <Text>Bank Account: 0954 4543 2112 3116</Text>
+          </View>
+          <View>
+            <Image
+              resizeMode="contain"
+              style={styles.withdrawImage}
+              source={require('./assets/withdraw.png')}
+            />
+          </View>
+        </View>
+        <View style={styles.balanceImage}>
+          <Image
+            resizeMode="contain"
+            style={styles.imageHW}
+            source={require('./assets/balance.png')}
+          />
+        </View>
+        <View style={styles.monthlySpendingImage}>
+          <Image
+            resizeMode="contain"
+            style={styles.imageHW}
+            source={require('./assets/monthly-spending.png')}
+          />
+        </View>
+        <View style={styles.totalSpendingImage}>
+          <Image
+            resizeMode="contain"
+            style={styles.imageHW}
+            source={require('./assets/total-spending.png')}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
-
 const styles = StyleSheet.create({
   container: {
-    margin: 20,
-    flex: 1,
-    justifyContent: "space-around"
+    padding: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    marginVertical: 20,
   },
-  headingTxt: {
-    fontSize: 24,
-    fontWeight: "bold",
-    padding: 2,
-    marginVertical: 12
-  }
+  currentBalance: {
+    fontSize: 36,
+    marginVertical: 5,
+  },
+  withdrawImage: {
+    width: 52,
+    height: 73,
+  },
+  balanceImage: {
+    backgroundColor: '#FFF',
+    height: 610,
+  },
+  monthlySpendingImage: {
+    backgroundColor: '#FFF',
+    height: 200,
+  },
+  totalSpendingImage: {
+    backgroundColor: '#FFF',
+    height: 600,
+  },
+  imageHW: {
+    width: '100%',
+    height: '100%',
+  },
 });
 export default LedgerGraphScreen;
