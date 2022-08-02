6b7dc8397dccdd4d86b89153c3c366d4a6b96c7a
Rana Shahzaib Ur Rehman <shazhaib.ali@crowdbotics.com>
2022-08-03 03:23:59 +0500

    Crowdbotics Studio: Update app screens

--- screens/Kanbanscreen/index.js
+++ screens/Kanbanscreen/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import { Image, Text, StyleSheet, View } from 'react-native';
 
 const KanbanScreen = params => {
-  return (
-    <View style={styles.container}>
-      <Image
-        resizeMode="contain"
-        style={styles.headerImage}
-        source={require('./assets/Header.png')}
-      />
+  return <View style={styles.container}>
+      <Image resizeMode="contain" style={styles.headerImage} source={require('./assets/Header.png')} />
       <Text style={styles.text}>Projects</Text>
       <View style={styles.tabView}>
         <View style={styles.selectedTab}>
@@ -23,24 +18,23 @@ const KanbanScreen = params => {
         <FileCard />
         <FileCard />
       </View>
-    </View>
-  );
+    </View>;
 };
 
 const styles = StyleSheet.create({
   container: {
     padding: 10,
     backgroundColor: '#FFF',
-    height: '100%',
+    height: '100%'
   },
   headerImage: {
     width: '100%',
-    height: 200,
+    height: 200
   },
   text: {
     color: '#1E2022',
     fontSize: 14,
-    fontWeight: '900',
+    fontWeight: '900'
   },
   tabView: {
     width: '60%',
@@ -50,7 +44,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 10,
     padding: 6,
-    marginVertical: 20,
+    marginVertical: 20
   },
   selectedTab: {
     borderRadius: 10,
@@ -60,7 +54,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     shadowColor: 'gray',
-    elevation: 10,
+    elevation: 10
   },
   tabItem: {
     width: '50%',
@@ -68,14 +62,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F1F1F1',
-    borderRadius: 10,
-  },
+    borderRadius: 10
+  }
 });
 export default KanbanScreen;
 
 const FileCard = () => {
-  return (
-    <View style={fileStyles.container}>
+  return <View style={fileStyles.container}>
       <View style={fileStyles.innerContainer}>
         <View style={fileStyles.img}>
           <Image source={require('./assets/Edit.png')} />
@@ -85,27 +78,24 @@ const FileCard = () => {
           <Text>18 June 2022, 10:30 AM</Text>
         </View>
         <View style={fileStyles.actionContainer}>
-          <Image
-            style={fileStyles.action}
-            source={require('./assets/Checkbox.png')}
-          />
+          <Image style={fileStyles.action} source={require('./assets/Checkbox.png')} />
         </View>
       </View>
-    </View>
-  );
+    </View>;
 };
+
 const fileStyles = StyleSheet.create({
   container: {
     borderRadius: 10,
     backgroundColor: '#FFF',
     shadowColor: 'gray',
     elevation: 8,
-    marginVertical: 5,
+    marginVertical: 5
   },
   innerContainer: {
     borderRadius: 10,
     flexDirection: 'row',
-    padding: 10,
+    padding: 10
   },
   img: {
     justifyContent: 'center',
@@ -113,21 +103,21 @@ const fileStyles = StyleSheet.create({
     backgroundColor: '#FCF1D6',
     height: 80,
     width: 80,
-    borderRadius: 10,
+    borderRadius: 10
   },
   actionContainer: {
     flex: 1,
     alignSelf: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   action: {
     justifyContent: 'center',
     alignItems: 'center',
     height: 40,
-    width: 40,
+    width: 40
   },
   textContainer: {
     paddingVertical: 10,
-    paddingHorizontal: 20,
-  },
+    paddingHorizontal: 20
+  }
 });
