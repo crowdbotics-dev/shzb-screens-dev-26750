0b361a3beeb4129573bfb36f783c691f15e019c9
Rana Shahzaib Ur Rehman <shazhaib.ali@crowdbotics.com>
2022-08-02 22:06:02 +0500

    Crowdbotics Studio: Update app screens

--- screens/Fileconverterscreen/index.js
+++ screens/Fileconverterscreen/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import {
-  Text,
-  StyleSheet,
-  View,
-  TextInput,
-  TouchableHighlight,
-  Image,
-  ScrollView,
-} from 'react-native';
+import { Text, StyleSheet, View, TextInput, TouchableHighlight, Image, ScrollView } from 'react-native';
 
 const FileConversion = params => {
-  return (
-    <ScrollView>
+  return <ScrollView>
       <View style={styles.container}>
         <View style={styles.main}>
           <View>
@@ -55,34 +46,33 @@ const FileConversion = params => {
           <Button>Convert</Button>
         </View>
       </View>
-    </ScrollView>
-  );
+    </ScrollView>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff'
   },
   main: {
-    padding: 20,
+    padding: 20
   },
   searchBar: {
     borderWidth: 1,
     borderRadius: 10,
     borderColor: '#C4C4C4',
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   searchText: {
     marginLeft: 10,
-    marginBottom: 10,
+    marginBottom: 10
   },
   wp90: {
-    width: '90%',
+    width: '90%'
   },
   text: {
-    marginVertical: 20,
+    marginVertical: 20
   },
   tabView: {
     width: '100%',
@@ -93,7 +83,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 10,
     paddingVertical: 6,
-    marginVertical: 20,
+    marginVertical: 20
   },
   csv: {
     borderRadius: 10,
@@ -101,7 +91,7 @@ const styles = StyleSheet.create({
     width: '15%',
     height: '100%',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   xlsx: {
     width: '15%',
@@ -109,26 +99,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F1F1F1',
-    borderRadius: 10,
+    borderRadius: 10
   },
   btn: {
     paddingHorizontal: '15%',
-    marginBottom: 20,
+    marginBottom: 20
   },
   bar: {
     marginHorizontal: '15%',
     marginVertical: 20,
     height: 8,
     backgroundColor: '#12D790',
-    borderRadius: 10,
-  },
+    borderRadius: 10
+  }
 });
-
 export default FileConversion;
 
 const FileCard = () => {
-  return (
-    <View style={fileStyles.container}>
+  return <View style={fileStyles.container}>
       <View style={fileStyles.innerContainer}>
         <View style={fileStyles.img}>
           <Image source={require('./assets/edit.png')} />
@@ -138,20 +126,20 @@ const FileCard = () => {
           <Text>18 June 2022</Text>
         </View>
       </View>
-    </View>
-  );
+    </View>;
 };
+
 const fileStyles = StyleSheet.create({
   container: {
     borderRadius: 10,
     padding: 10,
     shadowColor: 'rgba(0, 0, 0, 0.2)',
-    elevation: 5,
+    elevation: 5
   },
   innerContainer: {
     borderRadius: 10,
     flexDirection: 'row',
-    padding: 10,
+    padding: 10
   },
   img: {
     justifyContent: 'center',
@@ -159,38 +147,29 @@ const fileStyles = StyleSheet.create({
     backgroundColor: '#FCF1D6',
     height: 80,
     width: 80,
-    borderRadius: 10,
+    borderRadius: 10
   },
   textContainer: {
     paddingVertical: 10,
-    paddingHorizontal: 20,
-  },
+    paddingHorizontal: 20
+  }
 });
+
 const Button = props => {
-  return (
-    <TouchableHighlight onPress={props.onPress} underlayColor="#DDDDDD">
-      <View
-        style={[
-          btnStyles.button,
-          {
-            backgroundColor: props.backgroundColor
-              ? props.backgroundColor
-              : '#000000',
-            height: props.height ? props.height : 49,
-            borderWidth: props.borderWidth ? props.borderWidth : 0,
-            borderColor: props.borderColor ? props.borderColor : '#000000',
-          },
-        ]}>
-        <Text
-          style={[
-            btnStyles.text,
-            { color: props.color ? props.color : '#ffffff' },
-          ]}>
+  return <TouchableHighlight onPress={props.onPress} underlayColor="#DDDDDD">
+      <View style={[btnStyles.button, {
+      backgroundColor: props.backgroundColor ? props.backgroundColor : '#000000',
+      height: props.height ? props.height : 49,
+      borderWidth: props.borderWidth ? props.borderWidth : 0,
+      borderColor: props.borderColor ? props.borderColor : '#000000'
+    }]}>
+        <Text style={[btnStyles.text, {
+        color: props.color ? props.color : '#ffffff'
+      }]}>
           {props.children}
         </Text>
       </View>
-    </TouchableHighlight>
-  );
+    </TouchableHighlight>;
 };
 
 const btnStyles = StyleSheet.create({
@@ -198,30 +177,19 @@ const btnStyles = StyleSheet.create({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 10,
+    borderRadius: 10
   },
   text: {
     fontWeight: 'bold',
-    fontSize: 15,
-  },
+    fontSize: 15
+  }
 });
 
 const Input = props => {
-  return (
-    <View>
-      <TextInput
-        style={textStyles.input}
-        placeholder={props.placeholder}
-        value={props.value}
-        onChangeText={num => props.setValue(num)}
-        placeholderTextColor="#ddd"
-        editable={props.editable !== false}
-      />
-      {props.errorText ? (
-        <Text style={textStyles.error}>{props.errorText}</Text>
-      ) : null}
-    </View>
-  );
+  return <View>
+      <TextInput style={textStyles.input} placeholder={props.placeholder} value={props.value} onChangeText={num => props.setValue(num)} placeholderTextColor="#ddd" editable={props.editable !== false} />
+      {props.errorText ? <Text style={textStyles.error}>{props.errorText}</Text> : null}
+    </View>;
 };
 
 const textStyles = StyleSheet.create({
@@ -231,11 +199,11 @@ const textStyles = StyleSheet.create({
     color: '#000',
     borderRadius: 10,
     fontSize: 14,
-    paddingHorizontal: 10,
+    paddingHorizontal: 10
   },
   error: {
     fontSize: 13,
     color: '#FA060D',
-    paddingTop: 8,
-  },
+    paddingTop: 8
+  }
 });
