f70b5ffabc1286b3aa6f906027fcc36ba82faacb
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-04 05:36:47 +0500

    create appointment cleanup

--- screens/Createappointmentscreen/index.js
+++ screens/Createappointmentscreen/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Text,
   StyleSheet,
@@ -7,7 +7,7 @@ import {
   Image,
   FlatList,
   ScrollView,
-  Pressable,
+  Pressable
 } from "react-native";
 
 const CreateAppointmentScreen = params => {
@@ -15,68 +15,71 @@ const CreateAppointmentScreen = params => {
   const [location, setLocation] = useState("");
   const [description, setDescription] = useState("");
   const [reminder, setReminder] = useState("");
-  const [timeSlots, setTimeSlots] = useState([
-    {
-      id: 1,
-      slot: "10:00",
-      selected: false,
-    },
-    {
-      id: 2,
-      slot: "11:00",
-      selected: false,
-    },
-    {
-      id: 3,
-      slot: "12:00",
-      selected: true,
-    },
-    {
-      id: 4,
-      slot: "13:00",
-      selected: false,
-    },
-    {
-      id: 5,
-      slot: "14:00",
-      selected: false,
-    },
-    {
-      id: 6,
-      slot: "15:00",
-      selected: false,
-    },
-    {
-      id: 7,
-      slot: "16:00",
-      selected: false,
-    },
-    {
-      id: 8,
-      slot: "17:00",
-      selected: false,
-    },
-    {
-      id: 9,
-      slot: "18:00",
-      selected: false,
-    },
-    {
-      id: 10,
-      slot: "19:00",
-      selected: false,
-    },
-    {
-      id: 11,
-      slot: "20:00",
-      selected: false,
-    },
-    {
-      id: 12,
-      slot: "21:00",
-      selected: false,
-    },
-  ]);
+  const [timeSlots, setTimeSlots] = useState([]);
+  useEffect(() => {
+    setTimeSlots([
+      {
+        id: 1,
+        slot: "10:00",
+        selected: false
+      },
+      {
+        id: 2,
+        slot: "11:00",
+        selected: false
+      },
+      {
+        id: 3,
+        slot: "12:00",
+        selected: true
+      },
+      {
+        id: 4,
+        slot: "13:00",
+        selected: false
+      },
+      {
+        id: 5,
+        slot: "14:00",
+        selected: false
+      },
+      {
+        id: 6,
+        slot: "15:00",
+        selected: false
+      },
+      {
+        id: 7,
+        slot: "16:00",
+        selected: false
+      },
+      {
+        id: 8,
+        slot: "17:00",
+        selected: false
+      },
+      {
+        id: 9,
+        slot: "18:00",
+        selected: false
+      },
+      {
+        id: 10,
+        slot: "19:00",
+        selected: false
+      },
+      {
+        id: 11,
+        slot: "20:00",
+        selected: false
+      },
+      {
+        id: 12,
+        slot: "21:00",
+        selected: false
+      }
+    ]);
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -119,7 +122,7 @@ const CreateAppointmentScreen = params => {
               autoCorrect={false}
             />
             <Image
-              source={require("./assets/location.png")}
+              source={require("./assets/locationIcon.png")}
               style={styles.searchIcon}
             />
           </View>
@@ -158,15 +161,11 @@ const CreateAppointmentScreen = params => {
             numColumns={4}
             keyExtractor={item => item.id}
             columnWrapperStyle={{
-              justifyContent: "space-around",
+              justifyContent: "space-around"
             }}
           />
         </View>
-        <View style={styles.btnContainer}>
-          <Pressable style={styles.btn}>
-            <Text style={styles.btnText}>Create Appointment</Text>
-          </Pressable>
-        </View>
+        <Button buttonText={"Create Appointment"} onPress={() => {}} />
       </ScrollView>
     </View>
   );
@@ -174,12 +173,12 @@ const CreateAppointmentScreen = params => {
 
 const Slot = ({ slot }) => {
   const borderColor = {
-    borderColor: slot.selected ? "#12D790" : "#e6e6e6",
+    borderColor: slot.selected ? "#12D790" : "#e6e6e6"
   };
   return (
     <View style={[styles.slotPill, borderColor]}>
       <Text>{slot.slot}</Text>
-      <Image source={require("./assets/arrow.png")} style={styles.arrow} />
+      <Image source={require("./assets/arrowIcon.png")} style={styles.arrow} />
     </View>
   );
 };
@@ -187,7 +186,7 @@ const Slot = ({ slot }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#fff"
   },
   header: {
     flexDirection: "row",
@@ -197,37 +196,37 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     borderTopWidth: 1,
     borderBottomWidth: 1,
-    borderColor: "#ccc",
+    borderColor: "#ccc"
   },
   headerBlock: {
     flexDirection: "column",
     // justifyContent: 'center',
     alignItems: "center",
     justifyContent: "space-between",
-    height: 55,
+    height: 55
   },
   headerMainText: {
     fontSize: 22,
-    color: "#000",
+    color: "#000"
   },
   headerSubText: {
     fontSize: 14,
-    color: "grey",
+    color: "grey"
   },
   body: {
     height: 400,
     paddingHorizontal: 20,
-    marginTop: 10,
+    marginTop: 10
   },
   inputContainer: {
     flexDirection: "column",
     justifyContent: "center",
-    marginTop: 5,
+    marginTop: 5
   },
   inputText: {
     fontSize: 16,
     marginLeft: 20,
-    color: "#111112",
+    color: "#111112"
   },
   input: {
     borderWidth: 1,
@@ -236,26 +235,26 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     marginVertical: 5,
     width: "100%",
-    height: 50,
+    height: 50
   },
   textArea: {
-    height: 100,
+    height: 100
   },
   searchIcon: {
     position: "absolute",
     right: 20,
-    top: 40,
+    top: 40
   },
   timeSlots: {
     paddingHorizontal: 20,
-    paddingTop: 20,
+    paddingTop: 20
   },
   timeSlotsText: {
     fontSize: 16,
     marginLeft: 20,
     marginVertical: 5,
     color: "#111112",
-    fontWeight: "bold",
+    fontWeight: "bold"
   },
   slotPill: {
     flexDirection: "row",
@@ -266,18 +265,32 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: "#e6e6e6",
+    borderColor: "#e6e6e6"
   },
   arrow: {
     marginLeft: 10,
-    marginTop: 2,
-  },
+    marginTop: 2
+  }
+});
+export default CreateAppointmentScreen;
+
+const Button = ({ onPress, buttonText }) => {
+  return (
+    <View style={buttonStyles.btnContainer}>
+      <Pressable style={buttonStyles.btn} onPress={onPress}>
+        <Text style={buttonStyles.btnText}>{buttonText}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const buttonStyles = StyleSheet.create({
   btnContainer: {
     padding: 30,
     paddingTop: 10,
     paddingHorizontal: 40,
     justifyContent: "center",
-    marginTop: 20,
+    marginTop: 20
   },
   btn: {
     backgroundColor: "black",
@@ -289,12 +302,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     shadowColor: "rgba(0, 0, 0, 0.2)",
-    elevation: 10,
+    elevation: 10
   },
   btnText: {
     color: "#fff",
     fontSize: 16,
-    fontWeight: "bold",
-  },
+    fontWeight: "bold"
+  }
 });
-export default CreateAppointmentScreen;
