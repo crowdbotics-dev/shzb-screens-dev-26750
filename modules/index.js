53a733cf4b2cf9c50a4d4387a85a87e2fa518b4b
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-06 05:45:06 +0500

    lastest nav

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from "./*/index.js";
 import { getModules } from "./modules.js";
 
 export const modules = getModules(mods);
-export const initialRoute = "Searchlistingscreen";
+export const initialRoute = "Shippingaddressscreen";
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
