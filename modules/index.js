5d127ad9f87a9596de15f5fd75eecf927ccba344
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-07 02:18:14 +0500

    Walltescreen Added

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from "./*/index.js";
 import { getModules } from "./modules.js";
 
 export const modules = getModules(mods);
-export const initialRoute = "Videolistingscreen";
+export const initialRoute = "Walletscreen";
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
