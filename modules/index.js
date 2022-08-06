6f83d7b6f3ac90d896654bd0d994d1383114a4c2
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-06 23:12:09 +0500

    signature screen Added

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from "./*/index.js";
 import { getModules } from "./modules.js";
 
 export const modules = getModules(mods);
-export const initialRoute = "Shippingaddressscreen";
+export const initialRoute = "Signaturescreen";
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
