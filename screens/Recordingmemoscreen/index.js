5590df9b5131f6c7adf71d22c4280e8b4b8c58e1
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-10 02:22:35 +0500

    inline styling fixes

--- screens/Recordingmemoscreen/index.js
+++ screens/Recordingmemoscreen/index.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Text, StyleSheet, View, TextInput, Image, Pressable } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  Image,
+  Pressable
+} from "react-native";
 
 const RecordMemoScreen = params => {
   const [recordingName, setRecordingName] = useState("");
@@ -7,27 +14,38 @@ const RecordMemoScreen = params => {
   useEffect(() => {
     setRecordingTime("00:00:00");
   }, []);
-  return <View style={styles.container}>
+  return (
+    <View style={styles.container}>
       <View style={styles.inputContainer}>
         <Text style={styles.inputText}>Add name</Text>
-        <TextInput style={styles.input} onChangeText={text => setRecordingName(text)} value={recordingName} placeholder="Enter" placeholderTextColor="#9B9B9B" autoCapitalize="none" autoCorrect={false} />
+        <TextInput
+          style={styles.input}
+          onChangeText={text => setRecordingName(text)}
+          value={recordingName}
+          placeholder="Enter"
+          placeholderTextColor="#9B9B9B"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
       </View>
-      <Image source={require("./assets/recording.png")} style={styles.recordingImage} />
+      <Image
+        source={require("./assets/recording.png")}
+        style={styles.recordingImage}
+      />
       <Text style={styles.recordingTimeText}>{recordingTime}</Text>
       <View style={styles.buttonsContainer}>
         <Pressable style={styles.smallBtn}>
           <Image source={require("./assets/crossIcon.png")} />
         </Pressable>
         <Pressable style={styles.largeBtn}>
-          <Text style={{
-          color: "white"
-        }}>Start</Text>
+          <Text style={styles.whiteText}>Start</Text>
         </Pressable>
         <Pressable style={styles.smallBtn}>
           <Image source={require("./assets/tickIcon.png")} />
         </Pressable>
       </View>
-    </View>;
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -87,6 +105,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 50
+  },
+  whiteText: {
+    color: "#fff"
   }
 });
 export default RecordMemoScreen;
