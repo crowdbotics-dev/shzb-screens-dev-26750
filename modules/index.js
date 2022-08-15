9d1dde2b5d659e5d998ff0f21d14fb0e245bddfa
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-16 04:04:27 +0500

    diagnose symptoms screen addeed

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from "./*/index.js";
 import { getModules } from "./modules.js";
 
 export const modules = getModules(mods);
-export const initialRoute = "screenDiagnoseResultsLowRisk";
+export const initialRoute = "screenDiagnoseSymptoms";
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
