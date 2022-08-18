56b687a8267042e2732acf1569831218ca6b2af5
Shahzaib <shahzaibali1022@yahoo.com>
2022-08-18 07:45:31 +0500

    event home screen added

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from "./*/index.js";
 import { getModules } from "./modules.js";
 
 export const modules = getModules(mods);
-export const initialRoute = "screenEventDetails";
+export const initialRoute = "screenEventHome";
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
