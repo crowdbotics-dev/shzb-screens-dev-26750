6ea5235329df4347aa6f0ec9c67758e2f052105f
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-23 17:04:28 +0500

    ride booking successful screen added

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from "./*/index.js";
 import { getModules } from "./modules.js";
 
 export const modules = getModules(mods);
-export const initialRoute = "screenRewardsHistory";
+export const initialRoute = "screenRideBokingSuccessful";
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
