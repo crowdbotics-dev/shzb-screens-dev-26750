311d8a788b4cb5982a743be2bf57db877ce5037f
Shahzaib <shahzaibali1022@yahoo.com>
2022-08-23 20:26:57 +0500

    search hotels list screen added

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from "./*/index.js";
 import { getModules } from "./modules.js";
 
 export const modules = getModules(mods);
-export const initialRoute = "screenRideSharingHistory";
+export const initialRoute = "screenSearchHotelsList";
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
