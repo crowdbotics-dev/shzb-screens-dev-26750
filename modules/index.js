7553cde12c1f97366b90feb565c5361c916205d8
Shahzaib <shahzaibali1022@yahoo.com>
2022-08-25 13:38:00 +0500

    sponsor details screen added

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from "./*/index.js";
 import { getModules } from "./modules.js";
 
 export const modules = getModules(mods);
-export const initialRoute = "screenSetYourRate";
+export const initialRoute = "screenSponsorDetails";
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
