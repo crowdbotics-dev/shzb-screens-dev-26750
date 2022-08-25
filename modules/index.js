8cbe39b3404d3feaaf4a563ec4bd5feb5718ab23
Shahzaib <shahzaibali1022@yahoo.com>
2022-08-26 00:46:00 +0500

    task details screen added

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from "./*/index.js";
 import { getModules } from "./modules.js";
 
 export const modules = getModules(mods);
-export const initialRoute = "screenStripeRegistration";
+export const initialRoute = "screenTaskDetails";
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
