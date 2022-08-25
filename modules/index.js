a4ad9464d87ee17c1160cd2aaff397d44dc4aa7f
Shahzaib <shahzaibali1022@yahoo.com>
2022-08-25 22:01:30 +0500

    stripe registration screen added

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from "./*/index.js";
 import { getModules } from "./modules.js";
 
 export const modules = getModules(mods);
-export const initialRoute = "screenStripeProductList";
+export const initialRoute = "screenStripeRegistration";
 export const slices = modules
   .filter(mod => mod.value.slice)
   .map(mod => mod.value.slice);
